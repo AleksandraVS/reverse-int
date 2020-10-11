d11d914f374787975ac39f3be13946430b36c527
Sasha Vrublevskaya <Belarus>
2020-10-12 02:05:39 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
+  let str = String(Math.abs(n));
+  let temp = "";
+  for (let i = str.length-1; i >= 0; i--) 
+      temp += str[i]; 
   
+  return Number(temp);
 }
